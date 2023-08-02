51f91992b513c1777a1fa281e087a5ffe80f3479
Quan Vo <quan.vominh1@nashtechglobal.com>
2023-08-02 09:56:40 +0700

    Test - week 2

---
+++ Week2/Week2/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace Week2;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        //On<iOS>().SetUseSafeArea(false);
+    }
+
+    private void HambugerTapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+    }
+}
+
+
---
+++ Week2/Week2/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Platform;
+
+namespace Week2;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+		HandleEntryHandler();
+		return builder.Build();
+	}
+
+    public static void HandleEntryHandler()
+    {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+        {
+#if ANDROID
+            handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#elif IOS || MACCATALYST
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+            handler.PlatformView.FontWeight = Microsoft.UI.Text.FontWeights.Thin;
+#endif
+        });
+    }
+}
+
